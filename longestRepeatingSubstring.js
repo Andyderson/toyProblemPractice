060552f505fa67a943490953a1ba849c804bb7bc
Anderson Tsang <andyderson@gmail.com>
2018-11-18 22:35:40 -0800

    longestRepeatingSubstring toy problem

---
+++ longestRepeatingSubstring.js
@@ -0,0 +1,22 @@
+// Find the longest repeating substring of a string
+
+var repeatingSubstring = function(string) {
+  var result = "";
+  var sliced;
+  var index;
+
+  for (var i = 0; i < string.length; i++) {
+    if (string[i - 1] !== string[i]) {
+      index = i;
+    } else if (string[i - 1] === string[i]) {
+      sliced = string.slice(index, i + 1);
+
+      if (sliced.length > result.length) {
+        result = sliced;
+      }
+    }
+  }
+  return result;
+};
+
+repeatingSubstring("dbbbbbddddeeeeee"); //'ddddd'
