893bd51e57758e1a60cd1b7290603f7262659162
Anderson Tsang <andyderson@gmail.com>
2018-11-29 13:42:11 -0800

    wiggleSort third solution

--- wiggleSort.js
+++ wiggleSort.js
@@ -71,3 +71,22 @@ var wiggleSort = function(array) {
   }
   return array;
 };
+
+//Sorting
+
+var wiggleSortII = function(array) {
+  array.sort((a, b) => a - b);
+  var mid = Math.ceil(array.length / 2);
+  var firstHalf = array.slice(0, mid);
+  var secondHalf = array.slice(mid);
+
+  for (var i = 0; i < array.length; i += 2) {
+    array[i] = firstHalf.pop();
+  }
+
+  for (var j = 1; j < array.length; j += 2) {
+    array[j] = secondHalf.pop();
+  }
+
+  return array;
+};
