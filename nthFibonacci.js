f3b3163ce660611791abee93cf98bbb6e5bf0608
Anderson Tsang <andyderson@gmail.com>
2018-12-02 19:42:29 -0800

    nthFibonacci toy problem

---
+++ nthFibonacci.js
@@ -0,0 +1,48 @@
+/**
+ * A Fibonacci sequence is a list of numbers that begins with 0 and 1, and each
+ * subsequent number is the sum of the previous two.
+ *
+ * For example, the first five Fibonacci numbers are:
+ *
+ *   0 1 1 2 3
+ *
+ * If n were 4, your function should return 3; for 5, it should return 5.
+ *
+ * Write a function that accepts a number, n, and returns the nth Fibonacci
+ * number. Use a recursive solution to this problem; if you finish with time
+ * left over, implement an iterative solution.
+ *
+ * example usage:
+ * nthFibonacci(2); // => 1
+ * nthFibonacci(3); // => 2
+ * nthFibonacci(4); // => 3
+ * etc...
+ *
+ */
+
+//ForLoop
+var nthFibonacci = function(n) {
+  var number = [0, 1];
+  var pushValue = 0;
+
+  for (var i = 1; i < 20; i++) {
+    pushValue = number[i] + number[i - 1];
+    number.push(pushValue);
+  }
+  return number[n - 1] + number[n - 2];
+};
+
+//Recursion
+var nthFibonacci = function(n) {
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  return nthFibonacci(n - 1) + nthFibonacci(n - 2);
+};
+
+nthFibonacci(2); // => 1
+nthFibonacci(3); // => 2
+nthFibonacci(4); // => 3
