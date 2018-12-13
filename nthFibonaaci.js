6bfbc02dde45d2d55f416c3238d9f3cad321f8c6
Anderson Tsang <andyderson@gmail.com>
2018-12-13 09:42:42 -0800

    nthFibonacci Memoizaion and Bottom Up Solution

--- nthFibonaaci.js
+++ nthFibonaaci.js
@@ -7,4 +7,37 @@ var fibonacci = function(n) {
   return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
+// Memoization
+var fibonacci = function(n) {
+  var storage = [];
+  var result;
+
+  if (storage[n] !== undefined) {
+    return storage[n];
+  }
+
+  if (n <= 2) {
+    result = 1;
+  } else {
+    result = fibonacci(n - 1) + fibonacci(n - 2);
+    storage[n] = result;
+  }
+  return result;
+};
+
+//Bottom-Up
+var fibonacci = function(n) {
+  var storage = [0, 1, 1];
+
+  if (n <= 2) {
+    return 1;
+  }
+
+  for (var i = 3; i < n + 1; i++) {
+    storage[i] = storage[i - 1] + storage[i - 2];
+  }
+
+  return storage[n];
+};
+
 fibonacci(8); //21
