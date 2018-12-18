e8e0fe8bff83857b114563e5568817418ab4b19c
Anderson Tsang <andyderson@gmail.com>
2018-12-17 17:09:31 -0800

    integerToWords recursive solution

--- integerToWords.js
+++ integerToWords.js
@@ -236,6 +236,29 @@ var billionsDigits = function(num) {
   return result;
 };
 
+//Recursion
+var numberToWords = function(num) {
+  const to19 = `One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen`.split(
+      " "
+    ),
+    tens = `Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety`.split(" "),
+    toBil = `Thousand Million Billion`.split(" ");
+  let words = word(num);
+  words = words.split(/\s+/).join(" ");
+  return words ? words.trim() : "Zero";
+  function word(n) {
+    if (n < 20) return to19.slice(n - 1, n)[0] || "";
+    if (n < 100) return tens[Math.trunc(n / 10) - 2] + " " + word(n % 10);
+    if (n < 1000)
+      return to19[Math.trunc(n / 100) - 1] + " Hundred " + word(n % 100);
+    for (let i = 0, p = 1; i < toBil.length; p++, i++) {
+      if (n < 1000 ** (p + 1)) {
+        return word(n / 1000 ** p) + " " + toBil[i] + " " + word(n % 1000 ** p);
+      }
+    }
+  }
+};
+
 // numberToWords(0); //"Zero"
 // numberToWords(12); //"Twelve"
 // numberToWords(123); //"One Hundred Twenty Three"
