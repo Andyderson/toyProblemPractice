649ffb861f05daf19dc77a19fcf5668f4bc64b7b
Anderson Tsang <andyderson@gmail.com>
2018-12-18 21:59:36 -0800

    sumZero bruteforce solution

--- sumZero.js
+++ sumZero.js
@@ -8,6 +8,16 @@ var sumZero = function(array) {
   var positive = [];
   var negative = [];
 
+  for (var x = 0; x < array.length; x++) {
+    for (var y = 0; y < array.length; y++) {
+      for (var z = 0; z < array.length; z++) {
+        if (array[x] + array[y] + array[z] === 0) {
+          return true;
+        }
+      }
+    }
+  }
+
   for (var i = 0; i < array.length; i++) {
     if (array[i] > 0) {
       positive.push(array[i]);
@@ -36,5 +46,6 @@ var sumZero = function(array) {
 // sumZero([1, -1, -2]); //true
 // sumZero([0, 0, 0]); //true
 // sumZero([-1, -1, -1]); //false
-sumZero([1, -1, -5, 20, 2]); //true
-sumZero([1, 2, 3, 4, 5, 77, 8, -100]); //false
+// sumZero([1, -1, -5, 20, 2]); //true
+// sumZero([1, 2, 3, 4, 5, 77, 8, -100]); //false
+// sumZero([-1, -2, 3]); //true
