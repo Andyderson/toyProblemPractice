690eb3fd82bf344427d6d7ee7a800ece5ed98fe8
Anderson Tsang <andyderson@gmail.com>
2019-04-01 20:44:25 -0700

    numIslands toy problem

--- numIslands.js
+++ numIslands.js
@@ -1,43 +1,34 @@
 var numIslands = function(grid) {
-  let numIslands = 0;
+  var count = 0;
 
-  let recurse = function(x, y) {
+  var recurse = function(x, y) {
     grid[x][y] = "0";
+
     if (x > 0 && grid[x - 1][y] === "1") {
       recurse(x - 1, y);
     }
+
     if (y > 0 && grid[x][y - 1] === "1") {
       recurse(x, y - 1);
     }
+
     if (x < grid.length - 1 && grid[x + 1][y] === "1") {
       recurse(x + 1, y);
     }
+
     if (y < grid[0].length - 1 && grid[x][y + 1] === "1") {
       recurse(x, y + 1);
     }
   };
 
-  for (let x = 0; x < grid.length; x++) {
-    for (let y = 0; y < grid[0].length; y++) {
-      if (grid[x][y] === "1") {
-        recurse(x, y);
-        numIslands++;
+  for (var i = 0; i < grid.length; i++) {
+    for (var j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === "1") {
+        count++;
+        recurse(i, j);
       }
     }
   }
 
-  return numIslands;
+  return count;
 };
-
-numIslands([
-  ["1", "1", "0", "0", "0"],
-  ["1", "1", "0", "0", "0"],
-  ["0", "0", "1", "0", "0"],
-  ["0", "0", "0", "1", "1"]
-]);
-numIslands([
-  ["1", "1", "1", "1", "0"],
-  ["1", "1", "0", "1", "0"],
-  ["1", "1", "0", "0", "0"],
-  ["0", "0", "0", "0", "0"]
-]);
