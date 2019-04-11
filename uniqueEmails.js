dd40923446d9265e7775b791fb222e2831ccd418
Anderson Tsang <andyderson@gmail.com>
2019-04-10 17:02:49 -0700

    uniqueEmails alternate solution

--- uniqueEmails.js
+++ uniqueEmails.js
@@ -15,3 +15,23 @@ var numUniqueEmails = function(emails) {
   }
   return set.size;
 };
+
+//Alternate solution
+
+var numUniqueEmails = function(emails) {
+  var emailsArray = [];
+
+  for (var i = 0; i < emails.length; i++) {
+    var currEmail = emails[i];
+    var beforeAt = currEmail.split("@")[0];
+    var afterAt = currEmail.split("@")[1];
+
+    var localAddress = beforeAt.split("+")[0];
+    var removedPeriod = localAddress.replace(/[.]/gi, "");
+
+    var email = removedPeriod + "@" + afterAt;
+
+    emailsArray.push(email);
+  }
+  return [...new Set(emailsArray)].length;
+};
