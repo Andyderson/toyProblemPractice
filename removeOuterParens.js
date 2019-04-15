05adf2144a183d83822f0c93d1ff9e7210c17667
Anderson Tsang <andyderson@gmail.com>
2019-04-14 19:30:51 -0700

    removeOuterParens toy problem

---
+++ removeOuterParens.js
@@ -0,0 +1,12 @@
+var removeOuterParentheses = function(S) {
+  var results = [];
+  var count = 0;
+
+  for (var i = 0; i < S.length; i++) {
+    S[i] === "(" ? count++ : count--;
+    if ((S[i] === "(" && count != 1) || (S[i] === ")" && count != 0)) {
+      results.push(S[i]);
+    }
+  }
+  return results.join("");
+};
