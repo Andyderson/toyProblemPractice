56168aba7354e3bfa28963098eea72bf1f1db213
Anderson Tsang <andyderson@gmail.com>
2019-04-17 12:09:20 -0700

    minPathSum toy problem

---
+++ minPathSum.js
@@ -0,0 +1,31 @@
+// Input:
+// [
+//   [1,3,1],
+//   [1,5,1],
+//   [4,2,1]
+// ]
+// Output: 7
+// Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+
+var minPathSum = function(grid) {
+  var row = grid.length;
+  var column = grid[0].length;
+
+  for (var i = 0; i < row; i++) {
+    for (var j = 0; j < column; j++) {
+      if (i === 0 && j > 0) {
+        grid[i][j] += grid[i][j - 1];
+      }
+      if (i > 0 && j === 0) {
+        grid[i][j] += grid[i - 1][j];
+      }
+      if (i > 0 && j > 0) {
+        grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+      }
+    }
+  }
+
+  return grid[row - 1][column - 1];
+};
+
+minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]); //7
