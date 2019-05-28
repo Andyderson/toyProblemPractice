58afaa096171efc89744738f29e4f1a985167ffe
Anderson Tsang <andyderson@gmail.com>
2019-05-28 11:38:40 -0700

    findMaxAverage toy problem

---
+++ findMaxAverage.js
@@ -0,0 +1,19 @@
+// Input: [1,12,-5,-6,50,3], k = 4
+// Output: 12.75
+// Explanation: Maximum average is (12-5-6+50)/4 = 51/4 = 12.75
+
+var findMaxAverage = function(nums, k) {
+  var max = 0;
+  var sum = 0;
+
+  for (var i = 0; i < nums.length; i++) {
+    sum += nums[i] - (nums[i - k] || 0);
+
+    if (i >= k - 1) {
+      max = Math.max(max, sum);
+    }
+  }
+  return max / k;
+};
+
+console.log(findMaxAverage([1, 12, -5, -6, 50, 3], 4)); //12.75
