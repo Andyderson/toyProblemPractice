aa8af170c3b462f1b11c498919d585d801fc7067
Anderson Tsang <andyderson@gmail.com>
2019-07-01 21:28:45 -0700

    dota2Senate 2nd Solution

--- dota2Senate.js
+++ dota2Senate.js
@@ -2,64 +2,96 @@
  * @param {string} senate
  * @return {string}
  */
+
 var predictPartyVictory = function(senate) {
-  debugger;
-  var split = senate.split("");
+  var l = senate.length;
+  var senateArr = senate.split("");
+  var rIdxArr = [];
+  var dIdxArr = [];
 
-  for (var i = 0; i < split.length; i++) {
-    var indexD = split.indexOf("D"); // 1
-    var indexR = split.indexOf("R"); // 0
-
-    var indexD1 = split.indexOf("D", i + 1); // 1
-    var indexR1 = split.indexOf("R", i + 1); // -1
-
-    if (indexD === -1 && indexD1 === -1) {
-      return "Radiant";
+  senateArr.forEach((elem, idx) => {
+    if (elem === "R") {
+      rIdxArr.push(idx);
+    } else if (elem === "D") {
+      dIdxArr.push(idx);
     }
+  });
 
-    if (indexR === -1 && indexR1 === -1) {
-      return "Dire";
+  while (rIdxArr.length > 0 && dIdxArr.length > 0) {
+    if (dIdxArr[0] > rIdxArr[0]) {
+      rIdxArr.push(rIdxArr[0] + l);
+    } else {
+      dIdxArr.push(dIdxArr[0] + l);
     }
+    rIdxArr.shift();
+    dIdxArr.shift();
+  }
 
-    if (split[i] === "R" && indexD1 !== -1) {
-      split.splice(indexD1, 1);
-    }
-
-    if (split[i] === "D" && indexR1 !== -1) {
-      split.splice(indexR1, 1);
-    }
-
-    if (i === split.length - 1 && split[i] === "R" && indexD !== -1) {
-      split.splice(indexD, 1);
-    }
-
-    if (i === split.length - 1 && split[i] === "D" && indexR !== -1) {
-      split.splice(indexR, 1);
-    }
-
-    if (split.length === 1) {
-      if (split[0] === "R") {
-        return "Radiant";
-      } else {
-        return "Dire";
-      }
-    }
-
-    var indexD = split.indexOf("D"); // 1
-    var indexR = split.indexOf("R"); // 0
-
-    var indexD1 = split.indexOf("D", i + 1); // 1
-    var indexR1 = split.indexOf("R", i + 1); // -1
-
-    if (indexD === -1 && indexD1 === -1) {
-      return "Radiant";
-    }
-
-    if (indexR === -1 && indexR1 === -1) {
-      return "Dire";
-    }
+  if (dIdxArr.length !== 0) {
+    return "Dire";
+  } else {
+    return "Radiant";
   }
 };
+
+// var predictPartyVictory = function(senate) {
+//   debugger;
+//   var split = senate.split("");
+
+//   for (var i = 0; i < split.length; i++) {
+//     var indexD = split.indexOf("D"); // 1
+//     var indexR = split.indexOf("R"); // 0
+
+//     var indexD1 = split.indexOf("D", i + 1); // 1
+//     var indexR1 = split.indexOf("R", i + 1); // -1
+
+//     if (indexD === -1 && indexD1 === -1) {
+//       return "Radiant";
+//     }
+
+//     if (indexR === -1 && indexR1 === -1) {
+//       return "Dire";
+//     }
+
+//     if (split[i] === "R" && indexD1 !== -1) {
+//       split.splice(indexD1, 1);
+//     }
+
+//     if (split[i] === "D" && indexR1 !== -1) {
+//       split.splice(indexR1, 1);
+//     }
+
+//     if (i === split.length - 1 && split[i] === "R" && indexD !== -1) {
+//       split.splice(indexD, 1);
+//     }
+
+//     if (i === split.length - 1 && split[i] === "D" && indexR !== -1) {
+//       split.splice(indexR, 1);
+//     }
+
+//     if (split.length === 1) {
+//       if (split[0] === "R") {
+//         return "Radiant";
+//       } else {
+//         return "Dire";
+//       }
+//     }
+
+//     var indexD = split.indexOf("D"); // 1
+//     var indexR = split.indexOf("R"); // 0
+
+//     var indexD1 = split.indexOf("D", i + 1); // 1
+//     var indexR1 = split.indexOf("R", i + 1); // -1
+
+//     if (indexD === -1 && indexD1 === -1) {
+//       return "Radiant";
+//     }
+
+//     if (indexR === -1 && indexR1 === -1) {
+//       return "Dire";
+//     }
+//   }
+// };
 
 console.log(predictPartyVictory("RD")); //"Radiant"
 console.log(predictPartyVictory("RDD")); //"Dire"
