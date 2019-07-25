fc8b627729e9a15a5e080be03ac9ebeb3008fd88
Anderson Tsang <andyderson@gmail.com>
2019-07-25 11:43:46 -0700

    randomPWGenerator toy problem

---
+++ randomPWGenerator.js
@@ -0,0 +1,83 @@
+var getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var passwordGenerator = () => {
+  var bank = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    1,
+    2,
+    3,
+    5,
+    6,
+    7,
+    8,
+    9,
+    0
+  ];
+
+  var size = getRandomIntInclusive(6, 16);
+  var password = "";
+
+  for (var i = 1; i <= size; i++) {
+    var randomIndex = getRandomIntInclusive(0, 62);
+    password += bank[randomIndex];
+  }
+
+  return password;
+};
+
+console.log(passwordGenerator());
