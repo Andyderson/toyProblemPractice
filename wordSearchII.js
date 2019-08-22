47769348216d3729380fc906035c4c6babddd40f
Anderson Tsang <andyderson@gmail.com>
2019-08-21 20:54:30 -0700

    wordSearchII toy problem

--- wordSearchII.js
+++ wordSearchII.js
@@ -19,49 +19,51 @@ var findWords = function(board, words) {
   var word;
   var letter;
 
-  var bfs = (x, y, grid, currentWord) => {
-    // each conditional statement
-    // once element in matrix matches next letter in word
-    // concat element to tempString
-    // check if tempString matches a word in array of words
-    // if match, push into results, reset tempString to '' and iterate the for loop to look for the next word
-    if (x > 0 && grid[x - 1][y] === letter) {
-      bfs(x - 1, y);
-    }
-
-    if (y > 0 && grid[x][y - 1] === letter) {
-      bfs(x, y - 1);
-    }
-
-    if (x < grid.length - 1 && grid[x + 1][y] === letter) {
-      bfs(x + 1, y);
-    }
-
-    if (y < grid[0].length - 1 && grid[x][y + 1] === letter) {
-      bfs(x, y + 1);
+  var check = string => {
+    if (word === string) {
+      results.push(word);
+      tempString = "";
     }
   };
 
-  // for each word
+  var bfs = (x, y, grid, letterIdx) => {
+    //check left
+    if (y > 0 && grid[y - 1][x] === word[letterIdx]) {
+      tempString = tempString + word[letterIdx];
+      bfs(y - 1, x, grid, letterIdx + 1);
+    }
+
+    //check top
+    if (x > 0 && grid[y][x - 1] === word[letterIdx]) {
+      tempString = tempString + word[letterIdx];
+      bfs(y, x - 1, grid, letterIdx + 1);
+    }
+
+    //check right
+    if (x < grid.length - 1 && grid[x + 1][y] === word[letterIdx]) {
+      tempString = tempString + word[letterIdx];
+      bfs(x + 1, y, grid, letterIdx + 1);
+    }
+
+    //check bottom
+    if (y < grid[0].length - 1 && grid[x][y + 1] === word[letterIdx]) {
+      tempString = tempString + word[letterIdx];
+      bfs(x, y + 1, grid, letterIdx + 1);
+    }
+    check(tempString);
+  };
+
   for (var i = 0; i < words.length; i++) {
-    // loop through board length
     word = words[i];
-    for (var j = 0; j < words[i].length; j++) {
-      if (words[i][j + 1] !== undefined) {
-        letter = words[i][j + 1];
-      }
-      for (var k = 0; k < board.length; k++) {
-        // loop through board width
-        for (var l = 0; l < board[k].length; l++) {
-          // if letter of first word === elem on board
-          if (board[k][l] === words[i][0]) {
-            bfs(k, l, board, word);
-          }
+    for (var k = 0; k < board.length; k++) {
+      for (var l = 0; l < board[k].length; l++) {
+        if (board[k][l] === words[i][0]) {
+          tempString = tempString + words[i][0];
+          bfs(l, k, board, 1);
         }
       }
     }
   }
-
   return results;
 };
 
@@ -69,7 +71,7 @@ console.log(
   findWords(
     [
       ["o", "a", "a", "n"],
-      ["e", "t", "a", "e"],
+      ["k", "t", "a", "e"],
       ["i", "h", "k", "r"],
       ["i", "f", "l", "v"]
     ],
