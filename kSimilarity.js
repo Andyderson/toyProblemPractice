95bc49a392e3361e75f78e2a718293f1c7cbf41f
Anderson Tsang <andyderson@gmail.com>
2019-08-27 22:18:07 -0700

    kSimilarity alternate solution

--- kSimilarity.js
+++ kSimilarity.js
@@ -21,6 +21,37 @@ var kSimilarity = function(A, B) {
   return count;
 };
 
+var kSimilarity = function(A, B) {
+  var a = Array.from(A);
+  var b = Array.from(B);
+  var N = Infinity;
+
+  return dfs(0, a, b);
+
+  function dfs(p, a, b) {
+    if (p == a.length) return 0;
+
+    if (a[p] == b[p]) return dfs(p + 1, a, b);
+
+    var ans = N;
+
+    for (var i = p + 1; i < a.length; i++)
+      if (a[i] == b[p]) {
+        [a[i], a[p]] = [a[p], a[i]];
+        ans = Math.min(ans, dfs(p + 1, a, b) + 1);
+        [a[i], a[p]] = [a[p], a[i]];
+        if (b[i] == a[p]) break;
+      }
+    return ans;
+  }
+};
+
+// console.log(kSimilarity('ab', 'ba')); //1
+// console.log(kSimilarity('abc', 'bca')); //2
+// console.log(kSimilarity('abac', 'baca')); //2
+// console.log(kSimilarity('aabc', 'abca')); //2
+console.log(kSimilarity("bccaba", "abacbc")); //3
+
 // console.log(kSimilarity('ab', 'ba')); //1
 // console.log(kSimilarity('abc', 'bca')); //2
 // console.log(kSimilarity('abac', 'baca')); //2
