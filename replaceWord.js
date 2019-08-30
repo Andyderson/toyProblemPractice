9014a98039e07274e0dba4f2a0a02f1707c1cba6
Anderson Tsang <andyderson@gmail.com>
2019-08-29 23:42:54 -0700

    replaceWord toy problem

---
+++ replaceWord.js
@@ -0,0 +1,12 @@
+var replaceWords = function (dict, sentence) {
+    var split = sentence.split(" ");
+
+    for (ele of dict) {
+        for (i = 0; i < split.length; i++) {
+            if (split[i].slice(0, ele.length) === ele) {
+                split[i] = ele;
+            }
+        }
+    }
+    return split.join(" ");
+};
